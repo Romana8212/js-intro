b465426eab83a91926c95fa6d90605e0e40f8a3d
Romana8212 <romanafotoromana@gmail.com>
2020-04-02 21:12:44 +0000

    Aded text

---
+++ js/index.js
@@ -0,0 +1,13 @@
+ alert('Hello!');
+
+let greetings = 'World!';
+ alert(greetings);
+
+const message = 'Hello' + greetings;
+alert(message);
+
+const userAge = parseFloat(prompt('Enter your age'));
+alert('Your age is: ' + userAge);
+
+const nextAge = userAge + 1;
+alert(nextAge);
